59ab95e845f2b894354d48aff6e586d8c45c2d2b
Tharun0502 <105502721+Tharun0502@users.noreply.github.com>
2024-12-25 17:09:24 +0530

    Create Valid_Parentheses.cpp

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char it : s)
+        {
+            if(it == '[' || it == '(' || it == '{')
+            {
+                st.push(it);
+            }
+            else
+            {
+                if(st.size() == 0)
+                {
+                    return false;
+                }
+                char ch=st.top();
+                st.pop();
+                if((it ==')' && ch == '(') || (it == ']' && ch=='[') || (it == '}' && ch == '{'))
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
